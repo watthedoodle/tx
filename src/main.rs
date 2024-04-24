bf5770ae6db73989b08bc0762504c1b8106974b6
watthedoodle <watthedoodle@gmail.com>
2024-04-24 12:18:09 +0100

    first style completed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let genned = self::link_in_bio("wat the doodle!");
+    println!("{}", genned);
 }
-// ░L░I░N░К░ ░I░N░ В░I░О░ <-- example of an genned output
+
+fn link_in_bio(s: &str) -> String {
+    // ░L░I░N░К░ ░I░N░ В░I░О░
+    let mut genned = String::new();
+    for c in s.chars() {
+        let local = c.clone().to_string();
+        if c != ' ' {
+            genned.push_str(&"░");
+            genned.push_str(&local);
+        } else {
+            genned.push_str(&" ");
+        }
+    }
+    genned
+}
